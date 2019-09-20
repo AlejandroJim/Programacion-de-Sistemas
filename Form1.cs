914d7e197abf57db933e7dee2fb2895ed9d499ff
AlejandroJim <alejf18@hotmail.com>
2019-09-20 10:03:16 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_1
+{
+    public partial class Form1 : Form
+    {
+        Procesos pro = new Procesos();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string texto = pro.ExtraerIniciales(textBox1.Text);
+            int numero1 = Int32.Parse(textBox2.Text);
+            int numero2 = Int32.Parse(textBox5.Text);
+            textBox4.Text = ("" + texto);
+            textBox7.Text = (""+pro.CalculoMinimo(numero1,numero2));
+            textBox8.Text = ("" + pro.CalculoMaximo(numero1, numero2));
+        }
+    }
+}
---
+++ Procesos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_1
+{
+    class Procesos
+    {
+        public string ExtraerIniciales(string cadena)
+        {
+            String Ini=cadena.Substring(0,1);
+            for (int i=0;i<cadena.Length;i++)
+            {
+                if (cadena.Substring(i,1).Equals(" "))
+                {
+                    Ini += (""+ cadena.Substring(i+1,1));
+                }
+            }
+            return Ini;
+        }
+        public int CalculoMinimo(int n1,int n2)
+        {
+            return Math.Min(n1, n2);
+        }
+        public int CalculoMaximo(int n1, int n2)
+        {
+            return Math.Max(n1, n2);
+        }
+    }
+}
